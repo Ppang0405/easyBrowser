3234cf1a5cee78e5f173080eec19817ec008a911
Truong Nguyen <nguyentruong.xyz0405@gmail.com>
2020-09-02 16:52:08 +0700

    init project

--- EasyBrowser/ViewController.swift
+++ EasyBrowser/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        // ?
+        webView.navigationDelegate = self
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "Easy Browser"
+        
+        let url = URL(string: "https://www.youtube.com/watch?v=pLiT5DdjEbM")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
     }
 
 
